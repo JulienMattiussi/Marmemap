a18cc1e05ba27c4006b7346d100f37b218ef0bfd
JulienM <39904906+JulienMattiussi@users.noreply.github.com>
2021-06-30 21:21:21 +0200

    Second try scripts

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+var toiletsSound = WA.loadSound("flush.mp3");
+var config = {
+    volume : 0.5,
+    loop : false,
+    rate : 1,
+    detune : 1,
+    delay : 0,
+    seek : 0,
+    mute : false
+}
+// ...
+toiletsSound.stop();
+
 WA.onEnterZone('toilets', () => {
     WA.sendChatMessage("Hello!", 'Mr Robot');
-})
+});
+
+WA.onLeaveZone('toilets', () => {
+    toiletsSound.play(config);
+    WA.sendChatMessage("Hello!", 'Mr Robot');
+});
