b8a00b49061a7928f3b9197a1ace93371ccc45c4
JulienM <39904906+JulienMattiussi@users.noreply.github.com>
2021-06-30 21:49:36 +0200

    Working script

---
+++ script.js
@@ -0,0 +1,16 @@
+WA.closeCoWebSite();
+
+var toiletsSound = WA.loadSound("flush.mp3");
+var config = {
+    volume : 0.3,
+    loop : false,
+    rate : 1,
+    detune : 1,
+    delay : 0,
+    seek : 0,
+    mute : false
+}
+
+WA.onLeaveZone('toilets', () => {
+    toiletsSound.play(config);
+});
