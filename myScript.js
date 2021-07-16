548b81c9a8d012c7da4742e8a170547b7a0bee07
JulienMattiussi <39904906+JulienMattiussi@users.noreply.github.com>
2021-07-16 13:07:31 +0000

    Deploying to gh-pages from master @ 4d0902d45eb881cd54078cc553601be8d00091d8 🚀

--- myScript.js
+++ myScript.js
@@ -1,221 +1,227 @@
-const version = "0.7.12"
+const version = "0.7.13";
 
 console.log(`MarmeMap version : ${version}`);
 console.log(`WA API: ${Object.keys(WA)}`);
 
 const successBoard = {
-    wcPicture: {description: "Les photos des anciens dans les WC"},
-    superGenial: {description: "La vidéo du film légo dans la salle debian"},
-    babyShark: {description: "La vidéo de baby shark dans la douche"},
-    axe: {description: "La hache dans le coin du dressing"},
-    philippe: {description: "Philiiiipe après 5s d'attente au baby"},
-    intolerable: {description: "Parler 3 fois au voisin"},
+  wcPicture: { description: "Les photos des anciens dans les WC" },
+  superGenial: { description: "La vidéo du film légo dans la salle debian" },
+  babyShark: { description: "La vidéo de baby shark dans la douche" },
+  axe: { description: "La hache dans le coin du dressing" },
+  philippe: { description: "Philiiiipe après 5s d'attente au baby" },
+  intolerable: { description: "Parler 3 fois au voisin" },
 };
 let sucessPopup;
 let neighbourPopup;
 let philippeTimer;
-const philippeDelay = 5 //in seconds
+const philippeDelay = 5; //in seconds
 let intolerableCount = 0;
 
-
 const validateSuccess = (name) => {
-    let success = successBoard[name];
-    if (!success.valid) {
-        success.valid = true;
-    }
-}
+  let success = successBoard[name];
+  if (!success.valid) {
+    success.valid = true;
+  }
+};
 
 const getSuccessCount = () => Object.keys(successBoard).length;
 
-const getValidSuccessCount = () => 
-    Object.values(successBoard).filter(value => value.valid).length;
+const getValidSuccessCount = () =>
+  Object.values(successBoard).filter((value) => value.valid).length;
 
 const getSuccessList = () => {
-    const okList = Object
-        .values(successBoard)
-        .filter(value => value.valid)
-        .map(sucess => sucess.description)
-        .reduce((list, desc) => {
-            return `${list} / 
+  const okList = Object.values(successBoard)
+    .filter((value) => value.valid)
+    .map((sucess) => sucess.description)
+    .reduce((list, desc) => {
+      return `${list} /
             ${desc}`;
-        }, '');
-    return okList;
-}
+    }, "");
+  return okList;
+};
 
 const getToDoList = () => {
-    const todoList = Object
-        .keys(successBoard)
-        .filter(key => !successBoard[key].valid)
-        .reduce((list, name) => {
-            return `${list} / 
+  const todoList = Object.keys(successBoard)
+    .filter((key) => !successBoard[key].valid)
+    .reduce((list, name) => {
+      return `${list} /
             ${name} ???`;
-        }, '');
-    return todoList;
-}
+    }, "");
+  return todoList;
+};
 
 //TOILETS ZONE
-const toiletsSound = WA.loadSound("assets\/flush.mp3");
+const toiletsSound = WA.loadSound("assets/flush.mp3");
 const toiletsSoundConfig = {
-    volume : 0.1,
-    loop : false,
-    rate : 1,
-    detune : 1,
-    delay : 0,
-    seek : 0,
-    mute : false
-}
+  volume: 0.1,
+  loop: false,
+  rate: 1,
+  detune: 1,
+  delay: 0,
+  seek: 0,
+  mute: false,
+};
 
-WA.onLeaveZone('toiletsZone', () => {
-    toiletsSound.play(toiletsSoundConfig);
+WA.onLeaveZone("toiletsZone", () => {
+  toiletsSound.play(toiletsSoundConfig);
 });
 
 //WC PICTURE ZONE
-WA.onEnterZone('wcPictureZone', () => {
-    validateSuccess('wcPicture');
+WA.onEnterZone("wcPictureZone", () => {
+  validateSuccess("wcPicture");
 });
 
 //WC BABYSHARK ZONE
-WA.onEnterZone('babySharkZone', () => {
-    validateSuccess('babyShark');
+WA.onEnterZone("babySharkZone", () => {
+  validateSuccess("babyShark");
 });
 
 //WC SUPERGENIAL ZONE
-WA.onEnterZone('superGenialZone', () => {
-    validateSuccess('superGenial');
+WA.onEnterZone("superGenialZone", () => {
+  validateSuccess("superGenial");
 });
 
 //WC AXE ZONE
-WA.onEnterZone('axeZone', () => {
-    validateSuccess('axe');
+WA.onEnterZone("axeZone", () => {
+  validateSuccess("axe");
 });
 
 //WC PHILIPPE ZONE
-const philippeSound = WA.loadSound("assets\/philippe.mp3");
+const philippeSound = WA.loadSound("assets/philippe.mp3");
 const philippeSoundConfig = {
-    volume : 0.3,
-    loop : false,
-    rate : 1,
-    detune : 1,
-    delay : philippeDelay,
-    seek : 0,
-    mute : false
-}
+  volume: 0.3,
+  loop: false,
+  rate: 1,
+  detune: 1,
+  delay: philippeDelay,
+  seek: 0,
+  mute: false,
+};
 
-WA.onEnterZone('philippeZone', () => {
-    philippeTimer = Date.now();
-    philippeSound.play(philippeSoundConfig);
+WA.onEnterZone("philippeZone", () => {
+  philippeTimer = Date.now();
+  philippeSound.play(philippeSoundConfig);
 });
 
-WA.onLeaveZone('philippeZone', () => {
-    philippeSound.stop();
-    if (Date.now() > philippeTimer + philippeDelay*1000) {
-        validateSuccess('philippe');
-    }
+WA.onLeaveZone("philippeZone", () => {
+  philippeSound.stop();
+  if (Date.now() > philippeTimer + philippeDelay * 1000) {
+    validateSuccess("philippe");
+  }
 });
 
 //NEIGHBOUR ZONE
-WA.onEnterZone('intolerableZone', () => {
-    let intolerableText;
-    switch(intolerableCount) {
-        case 0: 
-            intolerableText = 'Vous pouriez dire bonjour !';
-            break;
-        case 1: 
-            intolerableText = 'Faites moins de bruit, merci !';
-            break;
-        default: 
-            intolerableText = 'Ca devient intolérable !!';
-            validateSuccess('intolerable');
-    }
-    neighbourPopup = WA.openPopup(
-        "intolerablePopup", 
-        intolerableText,
-        []
-    );
+WA.onEnterZone("intolerableZone", () => {
+  let intolerableText;
+  switch (intolerableCount) {
+    case 0:
+      intolerableText = "Vous pouriez dire bonjour !";
+      break;
+    case 1:
+      intolerableText = "Faites moins de bruit, merci !";
+      break;
+    default:
+      intolerableText = "Ca devient intolérable !!";
+      validateSuccess("intolerable");
+  }
+  neighbourPopup = WA.openPopup("intolerablePopup", intolerableText, []);
 });
 
-WA.onLeaveZone('intolerableZone', () => {
-    neighbourPopup.close();
-    intolerableCount ++;
+WA.onLeaveZone("intolerableZone", () => {
+  neighbourPopup.close();
+  intolerableCount++;
 });
 
 //SUCCESS BOARD
-WA.onEnterZone('successBoardZone', () => {
-    const successCount = getSuccessCount();
-    const validSuccessCount = getValidSuccessCount(); 
-    //WA.displayBubble();
-    sucessPopup = WA.openPopup(
-        "successBoardPopup", 
-        `SUCCESS BOARD
+WA.onEnterZone("successBoardZone", () => {
+  const successCount = getSuccessCount();
+  const validSuccessCount = getValidSuccessCount();
+  //WA.displayBubble();
+  sucessPopup = WA.openPopup(
+    "successBoardPopup",
+    `SUCCESS BOARD
 
-        Tu a découvert 
+        Tu a découvert
         ${validSuccessCount} succes
-        sur ${successCount}`, 
-        [{
-            label: "Next",
-            className: "primary",
-            callback: (popup) => {
-                popup.close(); 
-                sucessPopup = WA.openPopup(
-                    "successBoardPopup", 
-                    `SUCCESS BOARD
+        sur ${successCount}`,
+    [
+      {
+        label: "Next",
+        className: "primary",
+        callback: (popup) => {
+          popup.close();
+          sucessPopup = WA.openPopup(
+            "successBoardPopup",
+            `SUCCESS BOARD
 
                     ${getSuccessList()}
                     ${getToDoList()}
-                    `, 
-                    [{
-                        label: "Close",
-                        className: "primary",
-                        callback: (popup) => {
-                            popup.close();
-                        }
-                    }]
-                );
-            }
-        }]
+                    `,
+            [
+              {
+                label: "Close",
+                className: "primary",
+                callback: (popup) => {
+                  popup.close();
+                },
+              },
+            ]
+          );
+        },
+      },
+    ]
+  );
+
+  setTimeout(() => {
+    const style = document.createAttribute("style");
+    style.value = "white-space:pre-line;";
+    let popupDiv = document.getElementsByClassName(
+      "nes-container with-title is-centered"
     );
-
-    setTimeout(() => {
-        const style = document.createAttribute("style"); 
-        style.value = "white-space:pre-line;";
-        let popupDiv = document.getElementsByClassName("nes-container with-title is-centered");
-        console.log(popupDiv);
-        popupDiv[0] ? popupDiv[0].setAttributeNode(style) : null;     
-    }, 5000);
-    
+    console.log(popupDiv);
+    popupDiv[0] ? popupDiv[0].setAttributeNode(style) : null;
+  }, 5000);
 });
 
-WA.onLeaveZone('successBoardZone', () => {
-    //WA.removeBubble();
-    sucessPopup.close();
+WA.onLeaveZone("successBoardZone", () => {
+  //WA.removeBubble();
+  sucessPopup.close();
 });
 
-
-console.log('start fetch')
+console.log("start fetch");
 let myHeaders = new Headers();
-//myHeaders.append('Content-Type', 'application/json');
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Access-Control-Allow-Origin", "*");
+myHeaders.append("Origin", "*");
 
-let myInit = { method: 'GET',
-                headers: myHeaders,
-                mode: 'cors',
-                cache: 'default',
-                origin: '*' };
+let myInit = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+};
 
-let myRequest = new Request('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=2', myInit);
+let myRequest = new Request(
+  "https://cat-fact.herokuapp.com/facts?animal_type=cat",
+  //"https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=2",
+  myInit
+);
 
 fetch(myRequest, myInit)
-    .then((response) => {
-        console.log('p1');
-        console.log(response);
-        console.log(response.body);
-        return response;
-    })
-    .then((response) =>  response.json())
-    .then((response) => {
-        console.log('p2');
-        console.log(response);
-        console.log(response.body);
-    })
-    .catch((error) => {
-        console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
-    });
+  .then((response) => {
+    console.log("p1");
+    console.log(response);
+    console.log(response.body);
+    return response;
+  })
+  .then((response) => response.json())
+  .then((response) => {
+    console.log("p2");
+    console.log(response);
+    console.log(response.body);
+  })
+  .catch((error) => {
+    console.log(
+      "Il y a eu un problème avec l'opération fetch: " + error.message
+    );
+  });
